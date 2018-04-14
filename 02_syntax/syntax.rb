e7846774406cba6a7f076e9ed090e083699d5e1d
Jan Gloser <jan.renra.gloser@gmail.com>
2018-04-14 18:49:21 +0200

    Fixed ruby

--- 02_syntax/syntax.rb
+++ 02_syntax/syntax.rb
@@ -1,12 +1,7 @@
 pole = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
 
-#puts pole.inspect
-#puts pole[0].inspect
-
-for i in 0..2		
-  puts pole[i]
-
-  for j in 0..3
-    puts pole[j]
+for i in 0..pole.length
+  for j in 0..pole[i].length
+    puts pole[i][j]
   end
 end
